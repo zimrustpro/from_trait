c62b88e2e4c02e2afd51d66909db3eb05deab305
zimrustpro <zimrustpro@gmail.com>
2024-11-21 10:41:57 +0200

    `Vec::from()` examples

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let array_vec = Vec::from([8, 9, 10]);
+    println!("Vec from array: {array_vec:?}");
+
+    let str_vec = Vec::from("What kind of Vec am I?");
+    println!("Vec from strL {:?}", str_vec);
+
+    let string_vec = Vec::from("Wat will a String be?".to_string());
+    println!("Vec from String: {string_vec:?}");
 }
